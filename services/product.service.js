f2a0f851311170a565423e716f5342786b533550
Irving Canul <icanul@sol4it.net>
2024-05-04 12:27:23 -0600

    middlewares and validators

--- services/product.service.js
+++ services/product.service.js
@@ -1,4 +1,5 @@
 const { faker } = require('@faker-js/faker');
+const boom = require('@hapi/boom');
 
 class ProductsService {
 
@@ -15,7 +16,8 @@ class ProductsService {
         name: faker.commerce.productName(),
         description: faker.commerce.productDescription(),
         price: parseFloat(faker.commerce.price(), 10),
-        image: faker.image.url()
+        image: faker.image.url(),
+        isBlock: faker.datatype.boolean()
       })
     }
   }
@@ -39,14 +41,23 @@ class ProductsService {
   }
 
   async findOne(id){
-    return this.products.find(item => item.id === id);
+    const product =  this.products.find(item => item.id === id);
+
+    if(!product){
+      throw boom.notFound('Product not found');
+    }
+    if(product.isBlock){
+      throw boom.conflict('Product is blocked');
+    }
+
+    return product;
   }
 
   async update(id, changes){
     const index = this.products.findIndex(item => item.id === id);
 
     if(index === -1){
-      throw new Error('Product not found');
+      throw boom.notFound('Product not found');
     }
 
     const product = this.products[index];
@@ -62,7 +73,7 @@ class ProductsService {
     const index = this.products.findIndex(item => item.id === id);
 
     if(index === -1){
-      throw new Error('Product not found');
+      throw boom.notFound('Product not found');
     }
 
     this.products.splice(index, 1);
