26e3a2b2410a94e445ce3b73671dc80eb8d816fe
Irving Canul <icanul@sol4it.net>
2024-05-04 10:42:04 -0600

    http status

--- Routes/products.router.js
+++ Routes/products.router.js
@@ -21,11 +21,43 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const id = req.params.id;
+  if(id === '999'){
+    res.status(404).json({
+      message: 'Product not found'
+    });
+  } else {
+    res.status(200).json({
+      id,
+      name:'Producto 2',
+      price: 100
+    })
+  }
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.status(201).json({
+    message: 'created',
+    data: body
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
   res.json({
+    message: 'update',
+    data: body,
     id,
-    name:'Producto 2',
-    price: 100
-  })
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'delete',
+    id,
+  });
 });
 
 module.exports = router;
